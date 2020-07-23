547e6564a9d05d5009742aaffd9efb0f17f831bb
Mirica Yancey <myancey@fnal.gov>
2020-07-23 17:46:34 -0500

    Individualized Geo-Fence Creator

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var apiKey = '';
-var geofencingAdminKey = '';
-var geofencingProjectId = '';
+var apiKey = 'xhSLlv6eLXVggB5hbMeTK87voMmu2LV3';
+var geofencingAdminKey = '2dhUQXB09HvX7UlNWZ8M41NHyGwIiV5mGcmGFPQ0TvWcvUkJ';
+var geofencingProjectId = 'c669e2a6-bb95-4986-8341-b8f4312adb51';
